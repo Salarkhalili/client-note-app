8888b7df4441cb2da1e1b26e8da5fade1f939e92
amirgroot <amir1378khalili@gmail.com>
2024-02-28 16:04:59 +0330

    for salar

--- src/components/AddNoteForm.tsx
+++ src/components/AddNoteForm.tsx
@@ -1,35 +1,38 @@
-// import { useState } from "react";
+import { useState } from 'react';
 
-// import addNoteHook from "../hooks/addNoteHook";
+import { Box, Button, Input, Textarea } from '@chakra-ui/react';
+const AddNoteForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-// import { Alert, Box, Button, Input, Textarea } from "@chakra-ui/react";
-// const AddNoteForm = () => {
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
+  // این رو باید کامل کنی
+  //https://amir1.liara.run/api/v1/notes
+  // به لینک بالا با متد post
+  //درخواست بزن
+  // کد پایین اکیه فقط هندل کلیک رو درست کن
+  //   const handleAddNote = () => {
+  //     addNote();
+  //   };
 
-//   const handleAddNote = () => {
-//     addNote();
-//   };
+  return (
+    <Box>
+      <Input
+        type="text"
+        placeholder="عنوان یادداشت"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Textarea
+        placeholder="متن یادداشت"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        mt="4"
+      />
+      <Button colorScheme="blue" mt="4" onClick={handleAddNote}>
+        افزودن یادداشت
+      </Button>
+    </Box>
+  );
+};
 
-//   return (
-//     <Box>
-//       <Input
-//         type="text"
-//         placeholder="عنوان یادداشت"
-//         value={title}
-//         onChange={(e) => setTitle(e.target.value)}
-//       />
-//       <Textarea
-//         placeholder="متن یادداشت"
-//         value={content}
-//         onChange={(e) => setContent(e.target.value)}
-//         mt="4"
-//       />
-//       <Button colorScheme="blue" mt="4" onClick={handleAddNote}>
-//         افزودن یادداشت
-//       </Button>
-//     </Box>
-//   );
-// };
-
-// export default AddNoteForm;
+export default AddNoteForm;
